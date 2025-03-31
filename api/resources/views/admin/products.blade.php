1dfed1c363cdd15df2ccc1fa0ddf745bdf61f567
elchavez5000 <143212260+elchavez5000@users.noreply.github.com>
2025-03-31 12:57:18 -0600

    cambios de imagen

--- api/resources/views/admin/products.blade.php
+++ api/resources/views/admin/products.blade.php
@@ -82,7 +82,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body p-4">
-                <form action="/productos" method="POST" enctype="multipart/form-data">
+                <form action="/admin/products" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="row g-3">
                         <div class="col-md-6">
